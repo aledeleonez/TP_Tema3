72334000851761c0755d566fdc8bf8dcba311833
Pedro Manuel <98824525+PedroLopezG@users.noreply.github.com>
2022-02-14 17:08:09 +0000

    primeras lineas

--- main.java
+++ main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class main{
   public static void main (String[] args){
-    System.out.println("Hello world");
+    System.out.println("Introduzca la temperatura: ");
+    Scanner teclado = new Scanner(System.in);
+    int temp = teclado.nextInt();
+    System.out.println(temp);
+
+
+
+
   }
 }
