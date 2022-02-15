d29548aac8df5c96f0f77f74b59f2f4c8ae4b2c2
aledeleonez <98777405+aledeleonez@users.noreply.github.com>
2022-02-15 12:04:03 +0100

    Ejercicio 5

--- LaNomina.java
+++ LaNomina.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class LaNomina{
-    
+
+        public void nominaEmpleado(Scanner teclado){
+                System.out.println("Apellido del empleado: ");
+                String apellido = teclado.next();
+                System.out.println("Nombre del empleado: ");
+                String nombre = teclado.next();
+                System.out.println("Puesto del empleado: ");
+                System.out.println("1. Agente de servicio");
+                System.out.println("2. Empleado de oficina");
+                System.out.println("3. Directivo");
+                int puesto = teclado.nextInt();
+
+        }
 }
