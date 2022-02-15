c77d7b72cb44c55cf9748c23bc39f56cedc7c4b0
aledeleonez <98777405+aledeleonez@users.noreply.github.com>
2022-02-15 11:54:39 +0100

    Ejercicio 1 terminado

--- ElTiempo.java
+++ ElTiempo.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 public class ElTiempo{
 
     public double riesgoHielo(Scanner teclado){
-        System.out.println("Introduce la temperatura: ");
+        System.out.println("Introduce la temperatura en grados centígrados(ºC): ");
         double temperatura = teclado.nextFloat();
 
         if(temperatura < 2){
           System.out.println("!La temperatura es inferior a 2º¡ !Riesgo de hielo!");
+        }else{
+          System.out.println("No hay riesgo de hielo");
         }
     }
 }
